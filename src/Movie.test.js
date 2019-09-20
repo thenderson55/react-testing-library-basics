710f16853c57b0c27a84b3cb241010fd7cd0ed08
Thomas Henderson <tbhenderson55@yahoo.com>
2019-09-20 14:45:34 +0900

    tested links and imgs

--- src/Movie.test.js
+++ src/Movie.test.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import Movie from "./Movie";
+import Movie, { POSTER_PATH } from "./Movie";
 
 afterEach(() => {
-  cleanup
-  console.error.mockClear()
+  cleanup;
+  console.error.mockClear();
 });
 
 console.error = jest.fn();
-test("render movie", () => {
+test("render movie component", () => {
   // it will throw an error if there are no props
   render(<Movie />);
   expect(console.error).toHaveBeenCalled();
@@ -21,12 +21,17 @@ const movie = {
   poster_path: "jdiwojed.jpg"
 };
 
-test("render movie", () => {
-  render(
+test("render movie link", () => {
+  const { debug, getByTestId } = render(
     // have to wrap Link in something
     <MemoryRouter>
       <Movie movie={movie} />
     </MemoryRouter>
   );
+  debug();
+  // don't get absolute path (.href) , just the relative path
+  expect(getByTestId('movie-link').getAttribute('href')).toBe(`/${movie.id}`)
+  // src to get full absolute path
+  expect(getByTestId('movie-img').src).toBe(`${POSTER_PATH}${movie.poster_path}`)
   expect(console.error).not.toHaveBeenCalled();
 });
