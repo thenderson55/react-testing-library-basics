abe5455cb537690bf0fb35b5febc2f8ef48b9c32
Thomas Henderson <tbhenderson55@yahoo.com>
2019-09-20 20:45:37 +0900

    tested with mock axios api call

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -2,35 +2,49 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
 import { Poster } from './Movie';
+import axios from 'axios'
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 function MovieDetail (props) {
-  const [movie, setMovie] = useState({movie: ""})
-
+  const [movie, setMovie] = useState()
+  // const url = `https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=hi&language=en-US`
   useEffect(() => {
+    let mounted = true
     const fetchData = async () => {
       try {
-        const res = await fetch(`https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=hi&language=en-US`);
-        const movieData = await res.json();
-        setMovie({movie: movieData})
+        const res = await axios.get(props.url);
+        // const movieData = await res.json();
+        if(mounted){
+          setMovie(res.data)
+        }
+        console.log('move', res.data)
       } catch (e) {
         console.log(e);
       }
     }
     fetchData()
+
+    return () => {
+      mounted = false
+    }
   }, [])
 
+  if(!movie){
+    return <span data-testid="loading">Loading data ...</span>
+  }else{
 
+  }
   return (
+
     <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
       <MovieInfo>
         <Overdrive id={`${movie.id}`}>
           <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
         </Overdrive>
         <div>
-          <h1>{movie.title}</h1>
+          <h1 data-testid="resolved">{movie.title}</h1>
           <h3>{movie.release_date}</h3>
           <p>{movie.overview}</p>
         </div>
--- src/MovieDetail.test.js
+++ src/MovieDetail.test.js
@@ -1,30 +1,55 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import MovieDetail, {} from "./MovieDetail";
-
-global.fetch = require('jest-fetch-mock')
+import { render, cleanup, waitForElement } from "@testing-library/react";
+import MovieDetail from "./MovieDetail";
+import "@testing-library/jest-dom/extend-expect";
+// axios will detect the __mocks__ folder and use the mock
+// https://www.youtube.com/watch?time_continue=4&v=Ngj2f1n9pUw - tutorial!
+import axiosMock from "axios";
 
 afterEach(() => {
   cleanup;
-  console.error.mockClear();
+  // console.error.mockClear();
 });
-
-console.error = jest.fn();
-
 const match = {
   params: {
     id: "ihiu"
   }
-}
+};
 
-test('MovieDetail compoennt should render', () => {
-  fetch.mockResponseOnce(JSON.stringify({
-    movie: {
-      id: 'hpo',
-      title: 'LLLL',
-      backdrop_path: 'ohoefh'
+it("fetches and displays data", async () => {
+  axiosMock.get.mockResolvedValueOnce({
+    data: {
+      id: "hpo",
+      title: "LLLL",
+      backdrop_path: "ohoefh"
     }
-  }))
-  const { debug } = render(<MovieDetail match={match}/>)
-  debug()
+  });
+  const url = "/movie";
+  const { getByTestId } = render(<MovieDetail url={url} match={match} />);
+  expect(getByTestId("loading")).toHaveTextContent("Loading data ...");
+
+  const resolvedStuff = await waitForElement(() => getByTestId("resolved"))
+
+  expect(resolvedStuff).toHaveTextContent("LLLL")
+  expect(axiosMock.get).toHaveBeenCalledTimes(1)
+  expect(axiosMock.get).toHaveBeenCalledWith(url)
 });
+
+// global.fetch = require('jest-fetch-mock')
+
+// console.error = jest.fn();
+
+// test('MovieDetail compoennt should render', async () => {
+//   fetch.mockResponseOnce(JSON.stringify({
+
+//       id: 'hpo',
+//       title: 'LLLL',
+//       backdrop_path: 'ohoefh'
+
+//   }))
+//   const { debug, getByText } = render(<MovieDetail match={match}/>)
+//   await waitForElement(() => {
+//     getByText('LLLL')
+//   })
+//   debug()
+// });
